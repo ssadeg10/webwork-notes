f5d8490562f8280edb3b8b826ffd63732cf7ca7b
ssadeg10 <wildhunter360@gmail.com>
2021-12-13 01:39:39 -0800

    implemented saving notes to storage (text only)

--- webnote.js
+++ webnote.js
@@ -1,11 +1,7 @@
 
-// test to check if extension is loaded
-document.body.style.border = "5px solid red";
-
-// document.head.insertAdjacentHTML('beforeend',
-//     '<script>tinymce.init({selector: "#textArea"});</script>');
-
-
+if( document.readyState !== 'loading' ) {
+    restoreText();
+}
 
 const problemFooter = document.querySelector("#problemFooter");
 const p = problemFooter.getElementsByTagName('form')[0].lastChild;
@@ -19,22 +15,82 @@ webnoteButton.style.margin = "0 0.5ex"
 p.childNodes[0].style.margin = "0 0.5ex";
 p.appendChild(webnoteButton);
 
-// var mathSpan = document.createElement("span");
-// mathSpan.id = "mathSpan";
-// mathSpan.className = "mq-editable-field mq-math-mode";
-// mathSpan.style.width = "32.6px";
-// mathSpan.style.height = "20.1px";
-// mathSpan.style.padding = "4px 5px 2px 5px";
-
-// p.appendChild(mathSpan);
-// var mathField = MQ.MathField(mathSpan);
-
-// var form = document.createElement("form");
-// form.id = "parentForm";
-// p.appendChild(form);
+var textAreaDiv = document.createElement("div");
+textAreaDiv.id = "textAreaDiv";
+textAreaDiv.hidden = true;
+var hideBool = textAreaDiv.hidden;
+p.after(textAreaDiv);
 
 var textArea = document.createElement("textarea");
-// form.appendChild(textArea);
-p.after(textArea);
+textArea.id = "tiny";
+textArea.rows = "5";
+textArea.cols = "40";
+textAreaDiv.appendChild(textArea);
 
+var saveButton = document.createElement("input");
+saveButton.className = "webnote btn";
+saveButton.id = "saveButton"
+saveButton.value = "Save";
+saveButton.type = "button";
+saveButton.style.margin = "0 0.5ex";
+textAreaDiv.appendChild(saveButton);
 
+var saveNotif = document.createElement("div");
+saveNotif.className = "alert alert-success";
+saveNotif.role = "alert";
+saveNotif.innerText = "Your note has been saved";
+saveNotif.hidden = true;
+document.querySelector("#body-row").appendChild(saveNotif);
+
+webnoteButton.addEventListener("click", function(){
+    if(hideBool){
+        $("#textAreaDiv").slideDown();
+    } else {
+        $("#textAreaDiv").slideUp();
+    }
+    hideBool = !hideBool;
+    toggleHidden();
+});
+
+var textInputBool = false;
+
+textArea.addEventListener("input",  function(){
+    textInputBool = true;
+});
+
+$("#saveButton").click(function(){
+    if(textInputBool){
+        textInputBool = false;
+        $(".alert-success").slideDown().delay(4000).slideUp();
+    }
+});
+
+saveButton.addEventListener("click", function(){
+    saveText();
+});
+
+function toggleHidden(){
+    var textHidden = textAreaDiv.hidden;
+    textAreaDiv.hidden = !textHidden;
+}
+
+let breadcrumb = document.querySelector("ul.breadcrumb");
+let problemURL = breadcrumb.childNodes[breadcrumb.childNodes.length - 1].baseURI;
+var problemPath = problemURL.substr(0, problemURL.indexOf('?'));
+
+// url looses '?...' after checking answer resulting in an empty string
+if(problemPath === ''){ problemPath = problemURL }
+
+function saveText(e){
+    browser.storage.local.set({
+        [problemPath]: textArea.value
+    });
+    e.preventDefault();
+}
+
+function restoreText(){
+    var storageText = browser.storage.local.get(problemPath);
+    storageText.then((res) => {
+        textArea.value = res[problemPath] || '';
+    });
+}
